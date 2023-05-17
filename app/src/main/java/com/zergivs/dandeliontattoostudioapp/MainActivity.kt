088f9b81d4781186614721ca4c1c2f82b28428e3
zergivs <sergio.alvarado@rocketmail.com>
2023-05-16 21:31:14 -0400

    commit

--- app/src/main/java/com/zergivs/dandeliontattoostudioapp/MainActivity.kt
+++ app/src/main/java/com/zergivs/dandeliontattoostudioapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.zergivs.dandeliontattoostudioapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.buttonSignUp)
+        val button2: Button = findViewById(R.id.buttonSingIn)
+
+        button.setOnClickListener {
+            // Create an Intent to specify the destination activity
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
